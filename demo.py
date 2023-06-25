b8f48162a4e63b071e77483e061474d1a3533497
Jaemin Hong <hjm7392@mail.ulsan.ac.kr>
2023-06-26 03:44:16 +0900

    교수님 밋업

--- demo.py
+++ demo.py
@@ -11,28 +11,27 @@ tab1, tab2, tab3, tab4 = st.tabs(['🖊️ 소개', '🗂️ DS ▪️ DA ▪️
 
 with tab1:
     # 프로젝트 설명
-    st.write('''
-    Data Scientist의 역량을 알아보기 위해 wanted, saramin, Jobplanet, Incruit의 채용공고를      \n**데이터 사이언티스트, 데이터 애널리스트, 데이터 엔지니어**로 각각 수집하였다.
-    ''')
+    st.markdown('Data Scientist의 역량을 알아보기 위해 wanted, saramin, Jobplanet, Incruit의 채용공고를')
+    st.markdown('**데이터 사이언티스트, 데이터 애널리스트, 데이터 엔지니어**로 각각 수집하였습니다.')
 
     st.write('''
-    이 프로젝트의 최종 목표는
-    1. 원하는 직무의 역량을 제공해줄 수 있다.
-    2. 자신의 역량에 맞는 채용공고를 추천해줄 수 있다.
-    3. 특정 대기업의 채용공고만 추천해줄 수 있다.
+    이 앱은 3가지를 제공합니다.
+    1. 원하는 직무의 역량을 제공한다.
+    2. 자신의 역량에 맞는 채용공고를 추천한다.
+    3. 특정 대기업의 채용공고만 추천한다.
     ''')
 
     st.markdown('---')
 
 
     # 크롤링 진행 과정 
-    # video로 보여주기 (아님 사이트 사진, 코드 사진으로?)
     def file_upload(f):
         file_open = open(f'{f}', 'rb')
         file_read = file_open.read()
         return file_read
 
     st.subheader('채용공고 수집')
+    st.markdown('코드 보여주고, 돌아가는 모습을 보여줄 예정입니다.')
 
     st.markdown('#### 👇 wanted')
     f = 'C:\\Users\\LG\\Videos\\Captures\\wanted.mp4'
@@ -54,7 +53,49 @@ with tab1:
 
 
     # 텍스트 분석 진행 과정
-    # 코드 보여주고, 시각화한거 보여주기??
     st.subheader('텍스트 분석')
+    st.markdown('채용공고 수집과 마찬가지로, 코드를 보여주고 영상 밑에 워드클라우드를 보여줄 예정입니다.')
+    f = 'C:\\Users\\LG\\Videos\\Captures\\wanted.mp4'
+    st.video(file_upload(f))
     f = 'C:\\Users\\LG\\Videos\\Captures\\TA_EDA.png'
     st.image(file_upload(f))
+
+with tab2:
+    genre = st.radio(
+    "관심있는 키워드를 선택해주세요.",
+    ('데이터 사이언티스트', '데이터 애널리스트', '데이터 엔지니어'))
+
+    if genre == '데이터 사이언티스트':
+        st.write('''
+            개발 언어: Python, ...
+            \n역량: 영어 및 한글
+            \n데이터 사이언티스트에 맞는 채용공고를 추천해드릴게요!
+            \n[채용공고 (제목, 링크, 회사 위치)]
+        ''')
+
+    elif genre == '데이터 애널리스트':
+        st.write('''
+            개발 언어: Python, ...
+            \n역량: 영어 및 한글
+            \n데이터 애널리스트에 맞는 채용공고를 추천해드릴게요!
+            \n[채용공고 (제목, 링크, 회사 위치)]
+        ''')
+
+    else:
+        st.write('''
+            개발 언어: Python, ...
+            \n역량: 영어 및 한글
+            \n데이터 엔지니어에 맞는 채용공고를 추천해드릴게요!
+            \n[채용공고 (제목, 링크, 회사 위치)]
+        ''')
+
+with tab3:
+    options = st.multiselect(
+        '자신의 역량을 선택해주세요! (중복 선택 가능)',
+        ['정형데이터 분석', '이미지 분석', '텍스트 분석', '웹 제작', '의사소통']
+        )
+
+    st.write('나의 역량은 👇 ', options)
+
+with tab4:
+    st.markdown('오늘의 대기업 채용공고 입니다!')
